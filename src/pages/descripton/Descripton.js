7aa051cb12ca5f05619eef433a8c49cf8f58e210
nando <fmfernando61@gmail.com>
2021-07-24 21:06:37 -0300

    produção  componente descripton

--- src/pages/descripton/Descripton.js
+++ src/pages/descripton/Descripton.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { DescriptonContainer } from './DescriptonStyle';
+import image_2 from 'assets/img-2.jpg';
+
+import {
+  DescriptonContainer,
+  DescriptonContent,
+  DescriptonContent_text,
+  DescriptonContent_image,
+} from './DescriptonStyle';
 const Descripton = () => {
   return (
     <>
       <DescriptonContainer>
-        <h1>Descrição aqui</h1>
+        <DescriptonContent>
+          <DescriptonContent_text>
+            <h2 className="title">A vida não para</h2>
+            <p className="text">
+              A luta contra a pobreza não pode parar. Doando apenas R$ 2.00 por
+              dia você pode mudar a vida de uma criança e de toda sua
+              comunidade. Sua doação vai facilitar, por exemplo, que famílias
+              tenham acesso a direitos básicos, como à água e à alimentação
+              saudável.
+            </p>
+            <h2 className="title">Faça uma criança feliz</h2>
+          </DescriptonContent_text>
+          <DescriptonContent_image src={image_2} alt="children smile" />
+        </DescriptonContent>
       </DescriptonContainer>
     </>
   );
--- src/pages/descripton/DescriptonStyle.js
+++ src/pages/descripton/DescriptonStyle.js
@@ -1,14 +1,45 @@
 import styled from 'styled-components';
+//Photo by <a href="https://unsplash.com/@larm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Larm Rmah</a> on <a href="https://unsplash.com/s/photos/children?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
 
 export const DescriptonContainer = styled.section`
-  background-color: #620845;
+  background-color: #2ec4b6;
   width: 100%;
   height: 100vh;
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
-  .MainContainer_Button {
-    align-self: center;
+`;
+
+export const DescriptonContent = styled.div`
+  background-color: green;
+  display: flex;
+  width: 60%;
+  height: 60%;
+`;
+
+export const DescriptonContent_text = styled.div`
+  background-color: #011627;
+  width: 50%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title {
+    color: #ff9f1c;
+    font-size: 36px;
+    font-weight: bold;
   }
+  .text {
+    color: #fdfffc;
+    font-size: 24px;
+    font-weight: normal;
+  }
+`;
+
+export const DescriptonContent_image = styled.img`
+  border: solid 5px rgba(0, 0, 0, 0.5);
+  width: 50%;
+  height: auto;
 `;
